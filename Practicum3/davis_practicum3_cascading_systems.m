237acf83553b0cbee29ab6d74bd39592e9062015
griffindavis02 <davisgriffinp@gmail.com>
2022-03-10 09:11:40 -0500

    completed lab 3

--- Practicum3/davis_practicum3_cascading_systems.m
+++ Practicum3/davis_practicum3_cascading_systems.m
@@ -6,9 +6,18 @@ clc; clear; close all;
 
 %% Differential Equations for Systems
 syms s
-de = laplace((1/(s+2))*(3/((s+4)*(s+5))));
-sys1 = tf([1], [1 2]);
-sys2 = tf([3], conv([1 4], [1 5]));
-tf = sys1 * sys2;
+% sys1 = tf([1], [1 2]);
+% sys2 = tf([3], conv([1 4], [1 5]));
+% tf = sys1 * sys2;
 
+tf1 = 1/(s+2);
+de1 = ilaplace(tf1);
+pretty(de1)
 
+tf2 = 3/((s+4)*(s+5));
+de2 = ilaplace(tf2);
+pretty(de2)
+
+tf_net = tf1*tf2;
+de_net = ilaplace(tf_net);
+pretty(de_net)
